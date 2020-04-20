f44c760398222d1c05ea522f6f5dab40d79c1cc5
Kevin Müller <kevin.mueller1@outlook.com>
2020-04-20 17:01:46 +0200

    Added some documentation

--- Xamarin.Validation/ValidationExtension.cs
+++ Xamarin.Validation/ValidationExtension.cs
@@ -1,16 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
-using Xamarin.AttributeValidation.Attributes;
-using Xamarin.AttributeValidation.Helpers;
+﻿using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace Xamarin.AttributeValidation
 {
     public static class ValidationExtension
     {
+        /// <summary>
+        /// Validates the ViewModel and display the error messages in the view.
+        /// </summary>
+        /// <param name="page"></param>
+        /// <returns>
+        /// True: ViewModel is valid.
+        /// False: ViewModel is invalid.
+        /// </returns>
         public static async Task<bool> ValidateAsync(this Page page)
         {
             return await Validation.GetInstance(page).ValidateAsync(page);
